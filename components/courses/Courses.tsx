fbda5f2bc0331060dfd356d39b33f9d300a96679
Bigmanhackit <djhackit5@gmail.com>
2025-03-25 06:13:30 +0100

    course page update

--- components/courses/Courses.tsx
+++ components/courses/Courses.tsx
@@ -138,7 +138,7 @@ export default function Courses() {
                   fill
                   src={getThumbnailSrc(course.thumbnail)}
                   alt={course.title}
-                  className="object-cover"
+                  className="object-contain"
                   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 />
               </div>
